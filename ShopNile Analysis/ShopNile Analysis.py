3aab3e24b3d5c5a0b860d752d4fd465d0295547d
Mohab Ahmed <mohab.ahmed.ntss@gmail.com>
2026-03-09 01:59:17 +0200

    Update ShopNile Analysis.py

--- ShopNile Analysis/ShopNile Analysis.py
+++ ShopNile Analysis/ShopNile Analysis.py
@@ -10,11 +10,11 @@ st.title("🛍️ ShopNile Dashboard")
 @st.cache_data
 def load_data():
     base_path = os.path.dirname(os.path.abspath(__file__))
-    customers_df = pd.read_csv(os.path.join(base_path, "clean_data", "Customers_Clean.csv"))
-    products_df = pd.read_csv(os.path.join(base_path, "clean_data", "Products_Clean.csv"))
-    orders_df = pd.read_csv(os.path.join(base_path, "clean_data", "Orders_Clean.csv"))
-    payments_df = pd.read_csv(os.path.join(base_path, "clean_data", "Payments_Clean.csv"))
-    return customers_df, products_df, orders_df, payments_df
+    Dim_Customers_DF = pd.read_csv(os.path.join(base_path, "clean_data", "Customers_Clean.csv"))
+    Dim_Products_DF = pd.read_csv(os.path.join(base_path, "clean_data", "Products_Clean.csv"))
+    Fact_Orders_DF = pd.read_csv(os.path.join(base_path, "clean_data", "Orders_Clean.csv"))
+    Fact_Payments_DF = pd.read_csv(os.path.join(base_path, "clean_data", "Payments_Clean.csv"))
+    return Dim_Customers_DF, Dim_Products_DF, Fact_Orders_DF, Fact_Payments_DF
 # Merge DataFrames
 Orders_Payments_DF = pd.merge(Fact_Orders_DF, Fact_Payments_DF, on="order_id", how="inner")
 Orders_Customers_DF = pd.merge(Orders_Payments_DF, Dim_Customers_DF, on="customer_id", how="inner")
@@ -59,4 +59,5 @@ top_products = filtered_df.groupby("product_name")["amount_paid"].sum().reset_in
 fig_top_products = px.bar(top_products, x="product_name", y="amount_paid", title="Top 10 Products by Sales", labels={"amount_paid": "Total Sales"})
 
 st.plotly_chart(fig_top_products, use_container_width=True)
+
 
